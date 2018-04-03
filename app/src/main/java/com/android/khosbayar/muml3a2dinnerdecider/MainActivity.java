563a1ba1af3dc3a5486473eff6848b5e91671f95
hosoo1 <kbuyandalai@mum.edu>
2018-04-03 14:10:28 -0500

    Initial commit

---
+++ app/src/main/java/com/android/khosbayar/muml3a2dinnerdecider/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android.khosbayar.muml3a2dinnerdecider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
