70176238c668a59dff1c9f52470fe77f1a36cd3a
hosoo1 <kbuyandalai@mum.edu>
2018-04-03 14:12:42 -0500

    Updated

--- app/src/main/java/com/android/khosbayar/muml3a2dinnerdecider/MainActivity.java
+++ app/src/main/java/com/android/khosbayar/muml3a2dinnerdecider/MainActivity.java
@@ -2,12 +2,17 @@ package com.android.khosbayar.muml3a2dinnerdecider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mNewFood;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
